5ca06e417ec693d2b53805ffe0ed7cee6e34d0ea
April Jane Nadera <apriljanenadera154@gmail.com>
2025-09-29 15:01:17 +0800

    Add files via upload

---
+++ code_challenge10-1.py
@@ -0,0 +1,4 @@
+for J in range(11,0,-1):
+    for G in range(1, J ,1):
+        print("*", end=" ")
+    print()
---
+++ code_challenge10.py
@@ -0,0 +1,4 @@
+for A in range(1,11,1):
+    for K in range(1, A ,1):
+        print("*", end=" ")
+    print()
