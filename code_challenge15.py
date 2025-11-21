73a741ee4fc29ee17caabcb4e2661bc5c168ee25
April Jane Nadera <apriljanenadera154@gmail.com>
2025-11-21 13:45:47 +0800

    Add files via upload

---
+++ code_challenge15.py
@@ -0,0 +1,73 @@
+import os
+print('DLL STUDENT INFORMATION SYSTEM')
+print('==========================================')
+
+while True:
+    print("SELECT FROM THE FOLLOWING OPTION")
+    print("A - adding student record")
+    print("B - search student")
+    print("C - edit student record")
+    print("D - delete student record")
+    print("E - print all record")
+    print("F - export data")
+    print("G - exit system")
+
+    elite = input("enter here")
+
+    if elite == 'a':
+        print("ADD STUDENT RECORD")
+
+        student_id = input("Input student ID number")
+        first_name = input("Input student First Name ").upper()
+        last_name = input("Input student last name").upper()
+        
+        continue
+
+    elif elite == 'b':
+        print("SEARCH STUDENT")
+        
+        student_id = input("Input student ID number")
+        first_name = input("Input student First Name ").upper()
+        last_name = input("Input student last name").upper()
+        
+        continue
+
+    elif elite == 'c':
+        print("EDIT STUDENT RECORD")
+        
+        student_id = input("Input student ID number")
+        first_name = input("Input student First Name ").upper()
+        last_name = input("Input student last name").upper()
+        continue
+
+    elif elite == 'd':
+        print("DELETE STUDENT RECORD")
+        
+        student_id = input("Input student ID number")
+        first_name = input("Input student First Name ").upper()
+        last_name = input("Input student last name").upper()
+        continue
+
+    elif elite == 'e':
+        print("PRINT ALL RECORD")
+        
+        student_id = input("Input student ID number")
+        first_name = input("Input student First Name ").upper()
+        last_name = input("Input student last name").upper()
+        continue
+
+    elif elite == 'f':
+        print("EXPORT DATA")
+        
+        student_id = input("Input student ID number")
+        first_name = input("Input student First Name ").upper()
+        last_name = input("Input student last name").upper()
+        continue
+
+    elif elite == 'g':
+        print("EXIT SYSTEM")
+        
+        student_id = input("Input student ID number")
+        first_name = input("Input student First Name ").upper()
+        last_name = input("Input student last name").upper()
+        continue
